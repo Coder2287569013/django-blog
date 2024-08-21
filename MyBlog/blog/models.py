9486d616435d28bc6d1ad636507aa35e9bbe5d02
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-21 19:13:05 -0230

    Small changes

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.published_date}"
