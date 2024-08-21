9f1b9915b32e0ea1ee4f7afc93ecdde34ea78184
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-21 19:18:34 -0230

    Add template

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from blog import models
 # Create your views here.
+
+def posts_list(request):
+    posts = models.Post.objects.all()
+
+    context = {
+        "posts": posts
+    }
+
+    return render(request, "blog/posts-list.html", context)
